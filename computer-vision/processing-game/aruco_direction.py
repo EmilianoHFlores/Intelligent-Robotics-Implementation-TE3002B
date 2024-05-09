dcdcaa55611ce7bebf2e15382a425f3844461659
EmilianoHFlores <A00833160@tec.mx>
2024-05-08 23:26:45 -0600

    Complete 2D processing game with aruco

--- computer-vision/processing-game/aruco_direction.py
+++ computer-vision/processing-game/aruco_direction.py
@@ -1,29 +1,42 @@
 import cv2
-import cv2.aruco as aruco
 import numpy as np
 import os
 
-def findArucoMarkers(detector, img, markerSize = 6, totalMarkers=250, draw=True):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    key = getattr(aruco, f'DICT_{markerSize}X{markerSize}_{totalMarkers}')
-    bboxs, ids, rejected = detector.detectMarkers(gray)
-    return bboxs, ids, rejected
+import cv2.aruco as aruco
+import threading
+import socket
+import time
 
-def getPerpendicularAngle(point1, point2):
-    x1, y1 = point1
-    x2, y2 = point2
-    angle = np.arctan2(y2 - y1, x2 - x1)
-    return angle
+IP = "127.0.0.1"
+PORT = 5204
 
-def main():
-    cap = cv2.VideoCapture(0)
-    dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
-    parameters = cv2.aruco.DetectorParameters()
-    detector = cv2.aruco.ArucoDetector(dictionary, parameters)
-    
-    while True:
-        success, img = cap.read()
-        bboxes, ids, rejected = findArucoMarkers(detector, img)
+PUBLISH_RATE = 30
+
+ENABLE_SOCKET = True
+
+class arucoDetector:
+    def __init__(self, markerSize=6, totalMarkers=250):
+        self.markerSize = markerSize
+        self.totalMarkers = totalMarkers
+        self.dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
+        self.parameters = cv2.aruco.DetectorParameters()
+        self.detector = cv2.aruco.ArucoDetector(self.dictionary, self.parameters)
+
+    def findMarkers(self, img, draw=True):
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        bboxs, ids, rejected = self.detector.detectMarkers(gray)
+        return bboxs, ids, rejected
+
+    def getPerpendicularAngle(self, point1, point2):
+        x1, y1 = point1
+        x2, y2 = point2
+        angle = np.arctan2(y2 - y1, x2 - x1)
+        return angle
+
+    def processImage(self, img):
+        bboxes, ids, rejected = self.findMarkers(img)
+        angle = None
+        centroid = [-1, -1]
         if len(bboxes) > 0:
             for bbox, id in zip(bboxes, ids):
                 bbox = bbox[0]
@@ -36,22 +49,72 @@ def main():
                     point0 = (int(bbox[0][0]), int(bbox[0][1]))
                     point1 = (int(bbox[1][0]), int(bbox[1][1]))
                     # cv2.line(img, point0, point1, (255, 0, 255), 2)
-                    angle = getPerpendicularAngle(bbox[0], bbox[1])
+                    angle = self.getPerpendicularAngle(bbox[0], bbox[1])
                     print(angle)
                     # draw arrow from center of bbox to the center of the screen
                     center = (int(sum([x[0] for x in bbox]) / 4), int(sum([x[1] for x in bbox]) / 4))
+                    centroid = center
                     cv2.circle(img, center, 5, (0, 0, 255), cv2.FILLED)
                     arrow_tip = (int(center[0] + 50 * np.cos(angle)), int(center[1] + 50 * np.sin(angle)))
                     cv2.arrowedLine(img, center, arrow_tip, (0, 0, 255), 2)
                     
-                    
+                    # invert centroid x (as the camera is inverted)
+                    centroid = [img.shape[1] - centroid[0], centroid[1]]
 
-        cv2.imshow('img',img)
-        k = cv2.waitKey(30) & 0xff
-        if k == 27:
-            break
-    cap.release()
-    cv2.destroyAllWindows()
+        return img, centroid, angle
+
+class arucoPublisher:
+    def __init__(self):
+        self.IP = IP
+        self.PORT = PORT
+        self.RATE = PUBLISH_RATE
+        
+        self.centroid = [-1, -1]
+        self.angle = None
+        
+        if ENABLE_SOCKET:
+            self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            print("Waiting for connection...")
+            self.server.bind((self.IP, self.PORT))
+            self.server.listen(1)
+            self.client, addr = self.server.accept()
+            print(f"Connection from {addr}")
+            publish_thread = threading.Thread(target=self.publish)
+            self.active = True
+            publish_thread.start()
+        
+        self.main()
+        
+    def publish(self):
+        last_publish_time = time.time()
+        while self.active:
+            current_time = time.time()
+            if current_time - last_publish_time >= 1 / self.RATE:
+                if self.angle is not None:
+                    data = f"{self.centroid[0]},{self.centroid[1]},{self.angle}"
+                    self.client.send(data.encode())
+                    print(f"Published: {data}")
+                    last_publish_time = current_time
+    
+    def main(self):
+        cap = cv2.VideoCapture(0)
+        detector = arucoDetector()
+        
+        while self.active:
+            success, img = cap.read()
+            img, self.centroid, self.angle = detector.processImage(img)
+            
+            cv2.imshow('img', img)
+            k = cv2.waitKey(30) & 0xff
+            if k == 27:
+                self.active = False
+                break
+        
+        cap.release()
+        cv2.destroyAllWindows()
+
+def main():
+    arucoPublisher()
 
 if __name__ == "__main__":
     main()
--- computer-vision/processing-game/opencv_fit_rectangle_game/opencv_fit_rectangle_game.pde
+++ computer-vision/processing-game/opencv_fit_rectangle_game/opencv_fit_rectangle_game.pde
@@ -1,9 +1,33 @@
-// 
+import processing.net.*; 
+Client client;
+int port = 5204;
+String ip = "127.0.0.1";
+boolean VERBOSE = false;
+
 int rectSize = 50;
 int floorX, floorY, floorWidth, floorHeight;
+int rectX = 0; int rectY = 0;
 float rectAngle = 0;
 
-int floorHoleWidth, floorHoleHeight, floorX, floorY;
+int floorHoleWidth, floorHoleHeight, floorHoleX, floorHoleY;
+
+
+// IMAGE SIZE
+int imgWidth = 800;
+int imgHeight = 600;
+
+// a matrix where the collision points are stored
+boolean[][] collisionPoints = new boolean[imgWidth][imgHeight];
+boolean[][] rectCollisionPoints = new boolean[imgWidth][imgHeight];
+boolean collision;
+
+// Equations of the 4 sides of the rectangle, where the first element is the slope and the second is the y-intercept
+float rectEquations[][] = {
+  {1, 0},
+  {0, 1},
+  {1, 1},
+  {-1, 1}
+};
 
 void setup() {
   size(800, 600);
@@ -13,8 +37,29 @@ void setup() {
   floorHeight = height;
   floorHoleWidth = 60;
   floorHoleHeight = 100;
-  floorX = width / 2 - floorHoleWidth / 2;
-  floorY = floorY - floorHoleHeight;
+  floorHoleX = width / 2 - floorHoleWidth / 2;
+  floorHoleY = floorY + floorHoleHeight;
+  print("floorHoleY: " + floorHoleY + "\n");
+  print("floorY: " + floorY + "\n");
+  // set the collision points according to the floor
+  for (int i = 0; i < imgWidth; i++) {
+    for (int j = 0; j < imgHeight; j++) {
+      if (j < floorY || (j < floorHoleY && i > floorHoleX && i < floorHoleX + floorHoleWidth)) {
+        collisionPoints[i][j] = false;
+      } else {
+        collisionPoints[i][j] = true;
+      }
+    }
+  }
+  for (int i = 0; i < imgWidth; i++) {
+    for (int j = 0; j < imgHeight; j++) {
+      rectCollisionPoints[i][j] = false;
+    }
+  }
+
+  // Connect to the server's IP address and port
+  client = new Client(this, ip, port);
+  
 }
 
 // if keyboard is pressed, rotate the rectangle (+ or -)
@@ -29,8 +74,30 @@ void keyPressed() {
 
 void draw() {
   background(100, 200, 255);
+
+  // if client receives a message, print it
+  if (client.available() > 0) {
+    String message = client.readString();
+    print(message);
+    print("\n");
+    // message comes in format "x,y,angle"
+    String[] parts = split(message, ",");
+    rectX = int(parts[0]);
+    rectY = int(parts[1]);
+    rectAngle = float(parts[2]);
+  }
+  // draw collision points
+  // fill(0, 0, 0);
+  // for (int i = 0; i < imgWidth; i++) {
+  //  for (int j = 0; j < imgHeight; j++) {
+  //    if (collisionPoints[i][j]) {
+  //      point(i, j);
+  //    }
+  //  }
+  //  point(i, floorHoleY);
+  // }
   // Draw the floor
-  fill(50, 150, 200);
+  /*fill(50, 150, 200);
   rectMode(CORNER);
   rect(floorX, floorY, floorWidth, floorHeight);
 
@@ -45,7 +112,7 @@ void draw() {
   
   
   // Check if the mouse rectangle is inside the floor
-  if (rectLowestY < floorY || rectLowestX < floorX || rectHighestX > floorX + floorWidth) {
+  if (rectLowestY < floorY || rectLowestX < floorHoleX || rectHighestX > floorHoleX + floorWidth) {
         // Draw the rectangle at the mouse position
         fill(255, 255, 255); // Green
         noStroke();
@@ -65,6 +132,59 @@ void draw() {
     // draw rotated 45 degrees
     pushMatrix();
     translate(mouseX, mouseY);
+    rotate(rectAngle);
+    rect(0, 0, rectSize, rectSize);
+    popMatrix();
+  }*/
+
+  // Draw the floor
+  fill(50, 150, 200);
+  // no outline
+  noStroke();
+  rectMode(CORNER);
+  rect(floorX, floorY, floorWidth, floorHeight);
+  
+  // Draw the hole in the floor
+  fill(100, 200, 255); // Light blue
+  rect(floorHoleX, floorY, floorHoleWidth, floorHoleHeight);
+  // get rectangle collision points, considering rotation
+  for (int i = 0; i < imgWidth; i++) {
+    for (int j = 0; j < imgHeight; j++) {
+      rectCollisionPoints[i][j] = false;
+    }
+  }
+  for (int i = 0; i < rectSize; i++) {
+    for (int j = 0; j < rectSize; j++) {
+      int x = rectX - rectSize / 2 + i;
+      int y = rectY - rectSize / 2 + j;
+      int xRotated = (int) (rectX + (i - rectSize / 2) * cos(rectAngle) - (j - rectSize / 2) * sin(rectAngle));
+      int yRotated = (int) (rectY + (i - rectSize / 2) * sin(rectAngle) + (j - rectSize / 2) * cos(rectAngle));
+      if (xRotated >= 0 && xRotated < imgWidth && yRotated >= 0 && yRotated < imgHeight) {
+        rectCollisionPoints[xRotated][yRotated] = true;
+      }
+    }
+  }
+  // Draw the rectangle collisions
+  collision = false;
+  for (int i = 0; i < imgWidth; i++) {
+    for (int j = 0; j < imgHeight; j++) {
+      if (rectCollisionPoints[i][j] && collisionPoints[i][j]) {
+        stroke(255, 0, 0);
+        point(i, j);
+        collision = true;
+      }
+      else if (rectCollisionPoints[i][j]) {
+        stroke(0, 255, 0);
+        point(i, j);
+      }
+    }
+  }
+  if (!VERBOSE){
+    fill(255, 255, collision ? 0 : 255); // Green
+    noStroke();
+    rectMode(CENTER);
+    pushMatrix();
+    translate(rectX, rectY);
     rotate(rectAngle);
     rect(0, 0, rectSize, rectSize);
     popMatrix();
